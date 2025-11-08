ffc109aaee6becd2cc5c2bf09189d62c576e19c2
Mauricio0901 <anjos.mauricio@outlook.com>
2025-11-08 20:47:58 -0300

    colocando o import apicontroller

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,24 @@
 <template>
     <main>
+
+        <script>
+import ApiController from '../services/ApiController'; // ajuste o caminho conforme sua estrutura
+
+export default {
+  data() {
+    return {
+      email: '',
+      senha: ''
+    };
+  },
+  methods: {
+    async loginUsuario() {
+      await ApiController.login(this.email, this.senha);
+    }
+  }
+};
+</script>
+
         <div class="box">
             <div class="inner-box">
                 <!--Login-->

09be201f04b616fd07ad46602b53617049fec426
Mauricio0901 <anjos.mauricio@outlook.com>
2025-11-08 20:27:54 -0300

    atualização axios

--- src/ApiController.js
+++ src/ApiController.js
@@ -1,8 +1,7 @@
-import axios from 'axios';
-
-import Swal from "sweetalert2";
-const ApiUrl = '/api'; // usa o proxy do Netlify
-
+axios.post('/api/login', {
+  email: this.email,
+  senha: this.senha
+});
 
 export default {
 async login(email, senha) {
